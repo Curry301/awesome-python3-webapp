634b7c3eb2e3cc2a00dbddd0ce248ad60f781158
Ed-Jia <18124920792@163.com>
2017-09-24 16:26:22 +0800

    Create config_default.py

---
+++ config_default.py
@@ -0,0 +1,17 @@
+# -*-coding:utf-8 -*-
+
+__author__='Ed-Jia'
+
+configs={
+  'debug':True,
+  'db':{
+    'host':'127.0.0.1',
+    'port':3306,
+    'user':'www',
+    'password':'www',
+    'db':'awesome'
+  },
+  'session':{
+    'secret':'Awesome'
+  }
+}
